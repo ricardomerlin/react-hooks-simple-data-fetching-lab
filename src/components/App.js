497fe7a6b12095ee7b9ca797ea9163dad968d0f1
Ricardo Merlin <rcrdmerlin@gmail.com>
2023-11-27 13:37:52 -0500

    Uploading changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+
+
+import React, { useEffect, useState } from "react";
+
+function App () {
+
+const [dogPic, setDogPic] = useState('')
+
+console.log(dogPic)
+
+    useEffect(() => {
+        fetch ('https://dog.ceo/api/breeds/image/random')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.message)
+            setDogPic(data.message)
+        })
+    }, [])
+
+    if (!dogPic) return <p>Loading...</p>
+    else return <img src={dogPic} alt='A Random Dog' />
+}
+
+
+export default App
